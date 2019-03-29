298fd673e97877ac29da676727409ea79b5ba229
Yomna Hesham <yomnaismail216@gmail.com>
2019-03-29 23:52:23 +0200

    First Sprint

--- realestateproject/routes/web.php
+++ realestateproject/routes/web.php
@@ -15,3 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/profile','ProfileController@profile');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
